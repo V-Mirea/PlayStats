6ffcdc71fe72eba13334f26fb5c01a0f8786553c
SpeedOfSmell <vldmirea@gmail.com>
2019-02-25 21:33:25 -0500

    Start of moving from tkinter to qt

--- playstats/gui.py
+++ playstats/gui.py
@@ -1,70 +1,27 @@
-import tkinter as tk
-import PIL.Image, PIL.ImageTk
+import PyQt5.QtWidgets as qt
+from PyQt5 import QtGui, QtCore
+from analysis import AnalysisWindow_ui
 
 
-class MainWindow(tk.Tk):
+class AnalysisWindow(qt.QMainWindow):
     def __init__(self):
-        tk.Tk.__init__(self)
-        self.frame = None
+        super().__init__()
+        self.ui = AnalysisWindow_ui()
+        self.ui.setupUi(self)
 
-    def switch_frame(self, frame):
-        """Delete visible frame and replace with 'frame'."""
-        if self.frame is not None:
-            self.frame.destroy()
-        self.frame = frame
-        self.frame.pack(fill=tk.BOTH, expand=1)
+class VideoScreen(qt.QWidget):
+    def __init__(self, parent=None):
+        qt.QWidget.__init__(self, parent)
+        self.frame = QtGui.QImage()
 
-    def start(self, starting_screen=None):
-        starting_screen = starting_screen or SelectionScreen(self)
-        self.switch_frame(starting_screen)
-        self.mainloop()
+    def paintEvent(self, _):
+        painter = qt.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        self.paintImage = QtGui.QImage()
 
-
-class SelectionScreen(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-
-        self._selection_frame = tk.Frame(self)
-        self._path_entry = tk.Entry(self._selection_frame)
-        self._browse_button = tk.Button(self._selection_frame, text="...", command=self.browse)
-
-        self._selection_frame.grid(row=0, column=0, sticky="we")
-        self._path_entry.grid(row=0, column=0, sticky="we")
-        self._browse_button.grid(row=0, column=1)
-        self._selection_frame.columnconfigure(0, weight=1)
-
-        self._screen_frame = tk.Frame(self)
-        self._screen = tk.Text(self._screen_frame)
-
-        self._screen_frame.grid(row=1, column=0, sticky="nsew")
-        self._screen.grid(row=0, column=0, sticky="nsew")
-        self._screen_frame.columnconfigure(0, weight=1)
-        self._screen_frame.rowconfigure(0, weight=1)
-
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-
-    def browse(self):
-        pass
-
-
-class VideoScreen(tk.Frame):
-    def __init__(self, parent, stream=None):
-        tk.Frame.__init__(self, parent)
-        self.stream = stream
-
-        self.canvas = tk.Canvas(self, width=stream.width, height=stream.height)
-        self.canvas.pack(fill=tk.BOTH, expand=1)
-
-        self.delay = 15
-        self.update()
-
-    def update(self):
-        ret, frame = self.stream.get_frame()
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-
-        self.master.after(self.delay, self.update)
+    @QtCore.pyqtSlot(QtGui.QImage)
+    def setImage(self, image):
+        self.frame = image
+        self.paintEvent()
 
 
--- playstats/playstats.py
+++ playstats/playstats.py
@@ -1,16 +1,9 @@
 import gui
 import video
 
-import tkinter as tk
-import cv2
-
 
 def main():
-    cap = cv2.VideoCapture('res/BF4 HD test.avi')
-    stream = video.VideoStream(cap)
-    root = gui.MainWindow()
-    screen = gui.VideoScreen(root, stream)
-    root.start(screen)
+    screen = gui.AnalysisWindow()
 
 if __name__ == "__main__":
     main()
