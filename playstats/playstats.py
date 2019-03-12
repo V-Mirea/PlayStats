069daccb03de50d86f28e0cc77559575c53e35f2
SpeedOfSmell <vldmirea@gmail.com>
2019-03-12 11:39:43 -0400

    Remove pointless videocapture opbject

--- playstats/playstats.py
+++ playstats/playstats.py
@@ -3,9 +3,6 @@ import PyQt5.QtWidgets as qt
 import cv2
 
 def main():
-
-    cap = cv2.VideoCapture('res/BF3 C4 Jet Kill.mp4')
-
     app = qt.QApplication([])
     window = gui.AnalysisWindow()
     window.show()
